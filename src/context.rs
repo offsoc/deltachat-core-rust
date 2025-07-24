d5c418e909ddbadb6e030219bbb3e61c3b91a4ad
Hocuri <hocuri@gmx.de>
2025-07-24 17:24:54 +0200

    feat: Put the debug/release build version into the info (#7034)
    The info will look like:
    
    ```
    debug_assertions=On - DO NOT RELEASE THIS BUILD
    ```
    or:
    ```
    debug_assertions=Off
    ```
    
    I tested that this actually works when compiling on Android.
    
    <details><summary>This is how it looked before the second
    commit</summary>
    <p>
    
    
    The deltachat_core_version line in the info will look like:
    
    ```
    deltachat_core_version=v2.5.0 [debug build]
    ```
    or:
    ```
    deltachat_core_version=v2.5.0 [release build]
    ```
    
    I tested that this actually works when compiling on Android.
    
    
    
    </p>
    </details>

--- src/context.rs
+++ src/context.rs
@@ -333,6 +333,15 @@ impl Default for RunningState {
 /// about the context on top of the information here.
 pub fn get_info() -> BTreeMap<&'static str, String> {
     let mut res = BTreeMap::new();
+
+    #[cfg(debug_assertions)]
+    res.insert(
+        "debug_assertions",
+        "On - DO NOT RELEASE THIS BUILD".to_string(),
+    );
+    #[cfg(not(debug_assertions))]
+    res.insert("debug_assertions", "Off".to_string());
+
     res.insert("deltachat_core_version", format!("v{}", &*DC_VERSION_STR));
     res.insert("sqlite_version", rusqlite::version().to_string());
     res.insert("arch", (std::mem::size_of::<usize>() * 8).to_string());
